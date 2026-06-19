b885506f5fa39edcb5d455e5213f0b3f431de27d
janesh30 <janeshvaren30@gmail.com>
2026-06-19 09:17:54 +0800

    Delete restaurant_inventory_management_system/restaurant_inventory_management_system directory

--- restaurant_inventory_management_system/restaurant_inventory_management_system/src/main/java/com/css3402/project/restaurant_inventory_management_system/Repository/UserRepository.java
+++
@@ -1,9 +0,0 @@
-package com.css3402.project.restaurant_inventory_management_system.Repository;
-
-import com.css3402.project.restaurant_inventory_management_system.model.User;
-import org.springframework.data.jpa.repository.JpaRepository;
-import java.util.Optional;
-
-public interface UserRepository extends JpaRepository<User, Long> {
-    Optional<User> findByUsername(String username);
-}
